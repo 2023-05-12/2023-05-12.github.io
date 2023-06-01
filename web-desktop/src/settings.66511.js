b5e17e69b75ccd1e92a946e4eb55c67e432e2312
LinL <786662660@qq.com>
2023-06-01 22:04:29 +0800

    v3.1

--- web-desktop/src/settings.66511.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/scene/load.fire",orientation:"portrait",jsList:["assets/libs/ll.106eb.js"],bundleVers:{internal:"52d36",resources:"2e842","game-word":"13870",main:"bd118"}};
